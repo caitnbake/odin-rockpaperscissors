e3826987bac2affa7e09d1848ec6ce92c2521c69
caitnbake <caitlin.atterberry@gmail.com>
2024-11-03 15:28:14 -0800

    Add GUI

--- script.js
+++ script.js
@@ -9,49 +9,77 @@ let getComputerChoice = () => {
     }
 }
 
-let getHumanChoice = () => {
-    return humanChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+let playRound = (input) => {
+    let humanChoice = input;
+    let computerChoice = getComputerChoice();
+
+    switch (true) {
+        case (humanChoice === "rock" && computerChoice === "scissors") :
+        case (humanChoice === "scissors" && computerChoice === "paper") :
+        case (humanChoice === "paper" && computerChoice === "rock") :
+            humanScore += 1;
+            roundResults.innerText = `You win! ${humanChoice} beats ${computerChoice}.`;
+            break;
+        case (humanChoice === "rock" && computerChoice === "paper") :
+        case (humanChoice === "scissors" && computerChoice === "rock") :
+        case (humanChoice === "paper" && computerChoice === "scissors") :
+            computerScore += 1;
+            roundResults.innerText = `You lose! ${computerChoice} beats ${humanChoice}.`
+            break;
+        default :
+            roundResults.innerText = "It's a tie!";
+    }
+    score.innerText = `You: ${humanScore} | Computer: ${computerScore}`;
+    checkForWinner(humanScore, computerScore);
 }
 
-let playGame = () => {
-    let humanScore = 0;
-    let computerScore = 0;
+let reset = () => {
+    humanScore = 0;
+    computerScore = 0;
+    rockBtn.style.display = "inline";
+    paperBtn.style.display = "inline";
+    scissorsBtn.style.display = "inline";
+    resetBtn.style.display = "none";
+    gameResults.innerText = "";
+}
 
-    let playRound = (humanChoice, computerChoice) => {
-        switch (true) {
-            case (humanChoice === "rock" && computerChoice === "scissors") :
-            case (humanChoice === "scissors" && computerChoice === "paper") :
-            case (humanChoice === "paper" && computerChoice === "rock") :
-                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);    
-                humanScore += 1;
-                break;
-            case (humanChoice === "rock" && computerChoice === "paper") :
-            case (humanChoice === "scissors" && computerChoice === "rock") :
-            case (humanChoice === "paper" && computerChoice === "scissors") :
-                console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-                computerScore += 1;
-                break;
-            case (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") :
-                console.log("You entered an invalid answer. You lose");
-                computerScore += 1;
-                break;
-            default :
-                console.log("It's a tie!");
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const resetBtn = document.querySelector("#reset");
+
+const container = document.querySelector("#output");
+const roundResults = document.createElement("p");
+container.appendChild(roundResults);
+
+const scoreContainer = document.querySelector("#score");
+const score = document.createElement("p");
+scoreContainer.appendChild(score);
+
+const gameResults = document.createElement("h2");
+scoreContainer.appendChild(gameResults);
+
+let humanScore = 0;
+let computerScore = 0;
+
+rockBtn.addEventListener("click", () => playRound("rock"));
+paperBtn.addEventListener("click", () => playRound("paper"));
+scissorsBtn.addEventListener("click", () => playRound("scissors"));
+resetBtn.addEventListener("click", reset);
+
+let checkForWinner = (humanScore, computerScore) => {
+    if (humanScore === 5 || computerScore === 5) {
+        if (humanScore === 5) {
+            gameResults.innerText = `You won!`;
+        } else {
+            gameResults.innerText = `You lost.`;
         }
-        console.log(`You: ${humanScore} | Computer: ${computerScore}`);
-    }    
-
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (humanScore > computerScore) {
-        console.log("Final results: You win!!")
-    } else if (computerScore > humanScore) {
-        console.log("Final results: You lose!!")
-    } else {
-        console.log("Final results: Tie!")
-    }
+        gameResults.innerText += ` You: ${humanScore} | Computer: ${computerScore}`;
+        roundResults.innerText = "";
+        score.innerText = "";
+        rockBtn.style.display = "none";
+        paperBtn.style.display = "none";
+        scissorsBtn.style.display = "none";
+        resetBtn.style.display = "block";
+    } 
 }
-
-playGame();
